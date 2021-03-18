86ac311b445151f2773b086de645c76758d945e3
Scottly426 <lyscott123@gmail.com>
2021-03-17 18:34:18 -0700

    made changes for deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from './Pages/HomePage';
 import {Switch, Route} from 'react-router-dom';
 import ReactSnippets from './Pages/ReactSnippets';
 import VanillaJavascript from './Pages/VanillaJavascript';
-import BlogsPage from './Pages/BlogsPage';
 import ContactPage from './Pages/ContactPage';
 import { useState } from 'react';
 
@@ -36,9 +35,6 @@ function App() {
               </Route>
               <Route path="/vanillajavascript" exact>
                 <VanillaJavascript />
-              </Route>
-              <Route path="/blogs" exact>
-                <BlogsPage />
               </Route>
               <Route path="/contact" exact>
                 <ContactPage />
---
+++ src/Components/allAppClones.js
@@ -0,0 +1,60 @@
+import pacman from '../imgs/vanillajs/pacman.png';
+
+const appclones = [
+    {
+        id: 1,
+        link1: 'https://sl-pacman.netlify.app/',
+        image: pacman,
+        title: 'Pacman'
+    },
+    {
+        id: 2,
+        link1: 'https://sl-candycrush.netlify.app/',
+        image: candycrush,
+        title: 'Candy Crush'
+    },
+    {
+        id: 3,
+        link1: 'https://sl-minesweeper.netlify.app/',
+        image: minesweeper,
+        title: 'Mine Sweeper'
+    },
+    {
+        id: 4,
+        link1: 'https://sl-tetris.netlify.app/',
+        image: tetris,
+        title: 'Tetris'
+    },
+    {
+        id: 5,
+        link1: 'https://sl-2048.netlify.app/',
+        image: twentyfourtyeight,
+        title: '2048'
+    },
+    {
+        id: 6,
+        link1: 'https://sl-flappybird.netlify.app/',
+        image: flappybird,
+        title: 'Flappy Bird'
+    },
+    {
+        id: 7,
+        link1: 'https://sl-breakout.netlify.app/',
+        image: spaceinvaders,
+        title: 'Space Invaders'
+    },
+    {
+        id: 8,
+        link1: 'https://sl-spaceinvaders.netlify.app/',
+        image: breakout,
+        title: 'Breakout'
+    },
+    {
+        id: 9,
+        link1: 'https://sl-doodlejump.netlify.app/',
+        image: doodlejump,
+        title: 'Doodle Jump'
+    }
+]
+
+export default appclones;
