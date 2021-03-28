eccd54368743df0e4982556d9d0536c4951eb658
scottly426 <lyscott123@gmail.com>
2021-03-28 16:36:19 -0700

    adding more app clones

--- src/Components/allAppClones.js
+++ src/Components/allAppClones.js
@@ -1,8 +1,28 @@
 import airbnb from '../imgs/appclones/airbnb.png';
-
+import todoist from '../imgs/appclones/todoist.png';
+import discord from '../imgs/appclones/discord.png';
+import netflix from '../imgs/appclones/netflix.png';
 const appclones = [
     {
         id: 1,
+        link1: 'https://sl-todoist.netlify.app/',
+        image: todoist,
+        title: 'Todoist'
+    },
+    {
+        id: 2,
+        link1: 'https://sl-discord-clone.netlify.app/',
+        image: discord,
+        title: 'Discord'
+    },
+    {
+        id: 3,
+        link1: 'https://sl-netflix.netlify.app/browse',
+        image: netflix,
+        title: 'Netflix'
+    },
+    {
+        id: 4,
         link1: 'https://sl-airbnb-clone.netlify.app/',
         image: airbnb,
         title: 'Airbnb'
